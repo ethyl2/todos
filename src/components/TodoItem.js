f8ee76d971598751676fc570e58f7a0fa91fa211
Heather Nuffer <zzzuzz@gmail.com>
2020-09-04 11:58:03 -0600

    Refactor: Wraps todo items' emojis in spans for better accessibility

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,16 +5,32 @@ const TodoItem = (props) => {
     <div className="todo-item">
       {props.done ? (
         <p className="todo-circle" id={props.id} onClick={props.handleCheck}>
-          ✔️
+          <span
+            role="img"
+            aria-label="Check Mark"
+            id={props.id}
+            onClick={props.handleCheck}
+          >
+            ✔️
+          </span>
         </p>
       ) : (
         <p className="todo-circle" id={props.id} onClick={props.handleCheck}>
-          ❔
+          <span
+            role="img"
+            aria-label="White Question Mark Ornament"
+            id={props.id}
+            onClick={props.handleCheck}
+          >
+            ❔
+          </span>
         </p>
       )}
       <p className={props.done ? 'completed' : 'not-completed'}>{props.text}</p>
       <p className="delete-button" onClick={() => props.handleDelete(props.id)}>
-        ❌
+        <span role="img" aria-label="Cross Mark">
+          ❌
+        </span>
       </p>
     </div>
   );
